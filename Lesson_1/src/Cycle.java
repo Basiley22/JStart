1e6a8291a79cd6a96865febb1f15da43377e7688
Basiley <basileybas@gmail.com>
2020-11-14 20:32:20 +0100

    Добавлена задача 8 и мелкие исправления

---
+++ Lesson_1/src/Cycle.java
@@ -0,0 +1,40 @@
+public class Cycle {
+
+    public static void main(String[] args) {
+        int i;
+
+            //FOR. Print index for 0 to 20
+        System.out.println("\n*** FOR ***");
+        for (i = 0; i <= 20; i++) {
+            System.out.println(i);
+        }
+
+            //WHILE. Print pair numbers for 6 to -6
+        System.out.println("\n*** WHILE ***");
+        i = 6;
+        while (i >= -6) {
+            System.out.println(i);
+            i -= 2;
+        }
+
+            //DO - WHILE. Calculating the sum of odd numbers for start to end
+            //Немного доработал алгоритм,
+            //чтобы он принимал любые целочисленные границы.
+        System.out.println("\n*** DO WHILE ***");
+        int start = 10;
+        int end = 20;
+        int sum;
+
+        if (start % 2 == 0) start++;
+        if (end % 2 == 0) end--;
+        i = start;   //Технически, одна переменная лишняя. Но мне так показалось нагляднее.
+        sum = i;
+
+        do {
+            i += 2;
+            sum = sum + i;
+        } while (i < end);
+
+        System.out.println(" sum = " + sum);
+    }
+}
--- Lesson_1/src/Variable.java
+++ Lesson_1/src/Variable.java
@@ -5,7 +5,7 @@ public class Variable {
         short ramSizeGb = 8;                        //Объем оперативной памяти (ГБ)
         int windowsVersion = 1909;
         long capasityDiskByte = 239_155_015_680L;   //Емкость диска (Б)
-        float frequencyCPU = 1.8F;                  //Частота процессора (ГГц)
+        float frequencyCpuGHz = 1.8F;                  //Частота процессора (ГГц)
         double userPassword = 0.112_358_132_134;    //Пароль на вход
         char firstLetterName = 'B';                 //Первый символ логина
         boolean taskCompleted = true;               //Требуемое состояние задачи
