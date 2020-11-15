77bfcaa90bcecefdd58c319b3c7d35b2cdf99ad0
Basiley <basileybas@gmail.com>
2020-11-15 19:58:36 +0100

    ImitCalc

--- Lesson_1/src/ImitCalc.java
+++ Lesson_1/src/ImitCalc.java
@@ -7,23 +7,22 @@ public class ImitCalc {
     public static void main(String[] args) {
         int firstOperand = 4;
         int secOperand = 3;
-        double result;
         char signOp = '&';
 
-        if (signOp == '+') {                              //Addition
-            result = firstOperand + secOperand;
-            System.out.println(" Sum: " + result);
-        } else if (signOp == '-') {                       //Difference
-            result = firstOperand - secOperand;
-            System.out.println(" Difference: " + result);
-        } else if (signOp == '*') {                       //Multiply
-            result = firstOperand * secOperand;
-            System.out.println(" Product: " + result);
-        } else if (signOp == '/') {                       // Division
-            result = ((double) firstOperand / (double) secOperand);
-            System.out.println(" Quotient : " + result);
-        } else if (signOp == '^') {                       //Power
-            result = 1;
+        if (signOp == '+') {
+            System.out.println(" Sum: " + (firstOperand + secOperand));
+
+        } else if (signOp == '-') {
+            System.out.println(" Difference: " + (firstOperand - secOperand));
+
+        } else if (signOp == '*') {
+            System.out.println(" Product: " + (firstOperand * secOperand));
+
+        } else if (signOp == '/') {
+            System.out.println(" Quotient : " + ((double) firstOperand / (double) secOperand));
+
+        } else if (signOp == '^') {
+            int result = 1;
 
             for (int i = 1; i <= secOperand; i++) {
                 result *= firstOperand;
@@ -31,8 +30,8 @@ public class ImitCalc {
             System.out.println(" Power: " + result);
 
         } else if (signOp == '%') {                       //Mod division
-            result = firstOperand % secOperand;
-            System.out.println(" Mod division: " + result);
+            System.out.println(" Mod division: " + (firstOperand % secOperand));
+
         } else {
             System.out.println(" ! Invalid sign of operation: \'" + signOp +"\' !");
         }
