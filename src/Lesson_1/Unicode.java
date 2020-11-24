9c8825a21c101555592b4773f63426aaa16c2b7a
Basiley <basileybas@gmail.com>
2020-11-24 12:58:52 +0100

    Доработка Unicode

--- src/Lesson_1/Unicode.java
+++ src/Lesson_1/Unicode.java
@@ -3,18 +3,18 @@ public class Unicode {
      *Вывод символов Юникода в десятичном виде из указанного диапазона
      */
     public static void main(String[] args) {
-        short startRange = 33;
-        short endRange = 127;
+        int startRange = 33;
+        int endRange = 127;
         int columnsNumber = 15;
-        short codeSimbol = startRange;
-        while (codeSimbol <= endRange) {
+        char simbol = (char)startRange;
+        while (simbol <= endRange) {
             System.out.println();
-            for ( byte i = 1; i <= columnsNumber; i++) {
-                if (codeSimbol > endRange) {
+            for (int i = 1; i <= columnsNumber; i++) {
+                if (simbol > endRange) {
                     break;
                 }
-                System.out.print("  " + (char)codeSimbol);
-                codeSimbol++;
+                System.out.print("  " + simbol);
+                simbol++;
             }
         }
     }
